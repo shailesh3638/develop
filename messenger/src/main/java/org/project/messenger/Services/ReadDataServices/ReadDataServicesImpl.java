acb3995f68e56846856c1036872435baf5a81d6f
shailesh <shailesh3638@gmail.com>
2019-03-12 02:26:13 +0530

    datasource error persists

--- messenger/src/main/java/org/project/messenger/Services/ReadDataServices/ReadDataServicesImpl.java
+++ messenger/src/main/java/org/project/messenger/Services/ReadDataServices/ReadDataServicesImpl.java
@@ -6,11 +6,23 @@ import javax.ws.rs.Path;
 import org.project.messenger.Models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-
+import org.springframework.jdbc.object.StoredProcedure;
+import org.springframework.stereotype.Component;
+@Component
 @Path("/getuser")
-public class ReadDataServicesImpl implements ReadDataServices {
+public class ReadDataServicesImpl extends StoredProcedure implements ReadDataServices  {
 @Autowired
 	private DataSource dataSource;
+
+
+public DataSource getDataSource() {
+	return dataSource;
+}
+
+public void setDataSource(DataSource dataSource) {
+	this.dataSource = dataSource;
+}
+
 JdbcTemplate jdbcTemplate=new JdbcTemplate(dataSource);
 
 @Override
